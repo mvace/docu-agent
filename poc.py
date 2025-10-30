213ff83c1720691e82573770239a8831b235e142
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2025-10-30 18:06:16 +0100

    PDF loading and text splitting handled

--- poc.py
+++ poc.py
@@ -1,20 +1,20 @@
 import os
-from openai import OpenAI
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
 
 load_dotenv()
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
-API_KEY = os.environ.get("OPENAI_API_KEY")
+file = "sample.pdf"
+loader = PyPDFLoader(file)
+docs = loader.load()
 
-# client = OpenAI()
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=150, add_start_index=True
+)
+all_splits = text_splitter.split_documents(docs)
 
-# # 1) Vytvořit asistenta - dostane instrukce "jsi expert na..., odpovídej pouze na základě informací ze souboru"
-
-# assistant = client.beta.assistants.create(
-#     name="Expert na PDF",
-#     instructions="Jsi expert na dokumenty. Odpovídej pouze na základě informací, které najdeš v poskytnutých souborech. Pokud odpověď v souborech není, řekni, že to nevíš.",
-#     model="gpt-4o-mini",
-#     tools=[{"type": "file_search"}],
-# )
-
-# 2) Nahrání souboru a vytvoření vector store
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
